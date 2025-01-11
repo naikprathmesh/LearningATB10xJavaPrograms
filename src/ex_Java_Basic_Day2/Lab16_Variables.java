ce3929f6f6ed800a9529f42bc4cdc44e8ba33f8d
Prathamesh Naik <nkprathmesh@gmail.com>
2025-01-11 11:26:11 +0530

    11-Jan-2024

--- src/ex_Java_Basic_Day2/Lab16_Variables.java
+++ src/ex_Java_Basic_Day2/Lab16_Variables.java
@@ -2,8 +2,8 @@ package ex_Java_Basic_Day2;
 
 public class Lab16_Variables {
     public static void main(String[] args) {
-        int age = 1=60;
-        age=10+1;
+        int age = 10;
+        age =10+1;
         System.out.println(age);
     }
 }
---
+++ src/ex_Java_Basic_Day2/Lab17_Variables.java
@@ -0,0 +1,18 @@
+package ex_Java_Basic_Day2;
+
+public class Lab17_Variables {
+    public static void main(String[] args) {
+      int num = 9;
+        System.out.printf("9 x 1 = %d%n",num*1);
+        System.out.printf("9 x 2 = %d%n",num*2);
+        System.out.printf("9 x 3 = %d%n",num*3);
+        System.out.printf("9 x 4 = %d%n",num*4);
+        System.out.printf("9 x 5 = %d%n",num*5);
+        System.out.printf("9 x 6 = %d%n",num*6);
+        System.out.printf("9 x 7 = %d%n",num*7);
+        System.out.printf("9 x 8 = %d%n",num*8);
+        System.out.printf("9 x 9 = %d%n",num*9);
+        System.out.printf("9 x 10 = %d%n",num*10);
+
+    }
+}
