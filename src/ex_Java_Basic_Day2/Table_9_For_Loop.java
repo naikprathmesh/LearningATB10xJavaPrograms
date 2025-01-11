767ce32992f8910b68aaacf0a1a5c2cef509a3f4
Prathamesh Naik <nkprathmesh@gmail.com>
2025-01-11 11:30:57 +0530

    11-Jan-2024

---
+++ src/ex_Java_Basic_Day2/Table_9_For_Loop.java
@@ -0,0 +1,10 @@
+package ex_Java_Basic_Day2;
+
+public class Table_9_For_Loop {
+    public static void main(String[] args) {
+        int num = 9;
+        for (int i = 1; i < 11; i++) {
+            System.out.printf("%d x %d = %d%n",num,i,num*i);
+        }
+    }
+}
